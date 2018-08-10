96cf39394df5cbdf151275198f7635ffc1f388cc
hieunmce <hieunmce@gmail.com>
2018-08-10 10:01:47 +0700

    add example for CRUD

--- backend/handler/handler.go
+++ backend/handler/handler.go
@@ -5,7 +5,11 @@ import (
 	"net/http"
 
 	backendConfig "github.com/dwarvesf/smithy/backend/config"
+	"github.com/dwarvesf/smithy/backend/sqlmapper"
+	pgMapper "github.com/dwarvesf/smithy/backend/sqlmapper/pg"
+	"github.com/dwarvesf/smithy/common/database"
 	handlerCommon "github.com/dwarvesf/smithy/common/handler"
+	"github.com/k0kubun/pp"
 )
 
 // Handler handler for dashboard
@@ -26,6 +30,38 @@ func (h *Handler) NewUpdateConfigFromAgent() http.HandlerFunc {
 			handlerCommon.EncodeJSONError(err, w)
 			return
 		}
+
+		fmt.Fprintln(w, `{"status": "success"}`)
+	}
+}
+
+// NewCRUD .
+// FIXME: REMOVE
+// TODO: REMOVE and UPDATE FOR TMP ONLY
+func (h *Handler) NewCRUD() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		buf, err := pgMapper.NewQuerier(h.Config.GetDB(), "users", []database.Column{
+			{
+				Name: "id",
+				Type: "int",
+			},
+			{
+				Name: "name",
+				Type: "string",
+			},
+			{
+				Name: "age",
+				Type: "int",
+			},
+		}).Create(map[string]sqlmapper.ColData{
+			"name": sqlmapper.ColData{Name: "name", Data: "hieu"},
+			"age":  sqlmapper.ColData{Name: "name", Data: 26},
+		})
+		if err != nil {
+			pp.Print(err)
+		}
+		pp.Print(buf)
 
 		fmt.Fprintln(w, `{"status": "success"}`)
 	}

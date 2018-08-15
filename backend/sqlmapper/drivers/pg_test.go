0fa60791bcf9501b48515018d1565065814d3d0f
Phan Hieu <hieutrunglephan@gmail.com>
2018-08-15 10:36:56 +0700

    unit test findByColumnName

--- backend/sqlmapper/drivers/pg_test.go
+++ backend/sqlmapper/drivers/pg_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/dwarvesf/smithy/common/utils"
 )
 
-func Test_pgStore_FindAll(t *testing.T) {
+func beforeTest(t *testing.T) *backendConfig.Config {
 	// read config from agent
 	cfg, err := backendConfig.ReadYAML("../../../example_dashboard_config.yaml").Read()
 	if err != nil {
@@ -22,6 +22,13 @@ func Test_pgStore_FindAll(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Can't sync from agent %v", err)
 	}
+
+	return cfg
+}
+
+func Test_pgStore_FindAll(t *testing.T) {
+	// read yaml & sync config from agent
+	cfg := beforeTest(t)
 
 	//create sample data
 	sampleData := make(map[string]sqlmapper.ColData)
@@ -98,6 +105,94 @@ func Test_pgStore_FindAll(t *testing.T) {
 				id != tt.want[0]["id"].Data ||
 				name != tt.want[0]["name"].Data {
 				t.Errorf("pgStore.FindAll() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_pgStore_FindByColumnName(t *testing.T) {
+	// read yaml & sync config from agent
+	cfg := beforeTest(t)
+
+	//create sample data
+	sampleData := make(map[string]sqlmapper.ColData)
+	sampleData["id"] = sqlmapper.ColData{
+		Data:     999,
+		DataType: "int",
+	}
+	sampleData["name"] = sqlmapper.ColData{
+		Data:     "hieudeptrai",
+		DataType: "string",
+	}
+
+	tableName := "users"
+	cfg.DB().Exec(fmt.Sprintf("DELETE FROM %s;", tableName))
+	execQuery := fmt.Sprintf("INSERT INTO %s (%s) VALUES (%s) RETURNING id;",
+		tableName,
+		strings.Join([]string{"id", "name"}, ","),
+		"?,?")
+	cfg.DB().Exec(execQuery, sampleData["id"].Data, sampleData["name"].Data)
+
+	cols := []database.Column{
+		{
+			Name: "id",
+			Type: "int",
+		},
+		{
+			Name: "name",
+			Type: "string",
+		},
+	}
+
+	type args struct {
+		ColumnName string
+		Value      string
+		Offset     int
+		Limit      int
+	}
+
+	tests := []struct {
+		name    string
+		args    *args
+		want    []sqlmapper.RowData
+		wantErr bool
+	}{
+		{
+			name: "Valid test case",
+			args: &args{
+				ColumnName: "name",
+				Value:      "hieudeptrai",
+				Offset:     0,
+				Limit:      10,
+			},
+			want: []sqlmapper.RowData{sampleData},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := NewPGStore(cfg.DB(), tableName, cols, cfg.ModelList)
+			got, err := s.FindByColumnName(tt.args.ColumnName, tt.args.Value, tt.args.Offset, tt.args.Limit)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("pgStore.FindByColumnName() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			// convert data
+			iId, err := utils.ConvertFromInterfacePtr(got[0]["id"].Data)
+			if err != nil {
+				t.Fatal(err)
+			}
+			id := iId.(int)
+			iName, err := utils.ConvertFromInterfacePtr(got[0]["name"].Data)
+			if err != nil {
+				t.Fatal(err)
+			}
+			name := iName.(string)
+
+			if len(got) != len(tt.want) ||
+				id != tt.want[0]["id"].Data ||
+				name != tt.want[0]["name"].Data {
+				t.Errorf("pgStore.FindByColumnName() = %v, want %v", got, tt.want)
 			}
 		})
 	}

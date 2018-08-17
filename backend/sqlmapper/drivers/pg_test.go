6198ca484222323172af102fb228d17a5e1ef88e
Luan Ngo Minh <Ngominhluanbox@gmail.com>
2018-08-16 23:40:18 -0400

    Test delete, findbyid (#25)
    * Test delete, findbyid
    
    - Id valid
    - Id empty
    - Id not exist
    - Id too long
    
    * Change id too long
    
    - change to math.MaxInt32

--- backend/sqlmapper/drivers/pg_test.go
+++ backend/sqlmapper/drivers/pg_test.go
@@ -4,6 +4,7 @@ package drivers
 
 import (
 	"testing"
+	"math"
 
 	"github.com/dwarvesf/smithy/backend/sqlmapper"
 	"github.com/dwarvesf/smithy/common/database"
@@ -303,6 +304,249 @@ func Test_pgStore_FindByColumnName(t *testing.T) {
 					id != tt.want[i].Id ||
 					name != tt.want[i].Name {
 					t.Errorf("pgStore.FindByColumnName() = %v, want %v", got, tt.want)
+				}
+			}
+		})
+	}
+}
+
+func Test_pgStore_FindByID(t *testing.T) {
+	t.Parallel()
+	cfg, clearDB := utilTest.CreateConfig(t)
+	defer clearDB()
+
+	// migrate tables
+	err := utilTest.MigrateTables(cfg.DB())
+	if err != nil {
+		t.Fatalf("Failed to migrate table by error %v", err)
+	}
+
+	//create sample data
+	users, err := utilTest.CreateUserSampleData(cfg.DB())
+	if err != nil {
+		t.Fatalf("Failed to create sample data by error %v", err)
+	}
+
+	cols := []database.Column{
+		{
+			Name: "id",
+			Type: "int",
+		},
+		{
+			Name: "name",
+			Type: "string",
+		},
+	}
+
+	type args struct {
+		id int
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    sqlmapper.RowData
+		wantErr bool
+	}{
+		{
+			name:      "Valid test case",
+			tableName: "users",
+			args: &args{
+				id: 1,
+			},
+			want: users,
+		},
+		{
+			name:      "id not exists",
+			tableName: "users",
+			args: &args{
+				id: 111
+			},
+			wantErr: true,
+		}, 
+		{
+			name: "id too long",
+			tableName: "users",
+			args: &args {
+				id: id: math.MaxInt32 + 1,
+			}
+			wantErr: true
+		},
+		{
+			name: "id empty",
+			tableName: "users",
+			args: &args{
+				id: "",
+			}
+			wantErr: true
+		}
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var s sqlmapper.Mapper
+			if tt.testForEmptyTable {
+				cfgEmpty, clearDB := utilTest.CreateConfig(t)
+				defer clearDB()
+
+				// migrate tables
+				err := utilTest.MigrateTables(cfgEmpty.DB())
+				if err != nil {
+					t.Fatalf("Failed to migrate table by error %v", err)
+				}
+				s = NewPGStore(cfgEmpty.DB(), tt.tableName, cols, cfgEmpty.ModelList)
+			} else {
+				s = NewPGStore(cfg.DB(), tt.tableName, cols, cfg.ModelList)
+			}
+
+			got, err := s.FindByColumnName(tt.args.id)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("pgStore.FindByID() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if len(got) != len(tt.want) {
+				t.Errorf("len(got)=%v != len(tt.want)=%v", len(got), len(tt.want))
+				return
+			}
+
+			for i := 0; i < len(got); i++ {
+				// convert data
+				iId, err := utilReflect.ConvertFromInterfacePtr(got[i]["id"].Data)
+				if err != nil {
+					t.Fatal(err)
+				}
+				id := iId.(int)
+				iName, err := utilReflect.ConvertFromInterfacePtr(got[i]["name"].Data)
+				if err != nil {
+					t.Fatal(err)
+				}
+				name := iName.(string)
+
+				if len(got) != len(tt.want) ||
+					id != tt.want[i].Id ||
+					name != tt.want[i].Name {
+					t.Errorf("pgStore.FindByID() = %v, want %v", got, tt.want)
+				}
+			}
+		})
+	}
+}
+
+func Test_pgStore_Delete(t *testing.T) {
+	t.Parallel()
+	cfg, clearDB := utilTest.CreateConfig(t)
+	defer clearDB()
+
+	// migrate tables
+	err := utilTest.MigrateTables(cfg.DB())
+	if err != nil {
+		t.Fatalf("Failed to migrate table by error %v", err)
+	}
+
+	//create sample data
+	users, err := utilTest.CreateUserSampleData(cfg.DB())
+	if err != nil {
+		t.Fatalf("Failed to create sample data by error %v", err)
+	}
+
+	cols := []database.Column{
+		{
+			Name: "id",
+			Type: "int",
+		},
+		{
+			Name: "name",
+			Type: "string",
+		},
+	}
+
+	type args struct {
+		id int
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name:      "Valid test case",
+			tableName: "users",
+			args: &args{
+				id: 1,
+			},
+			want: users,
+		},
+		{
+			name:      "id not exists",
+			tableName: "users",
+			args: &args{
+				id: 111
+			},
+			wantErr: true,
+		}, 
+		{
+			name: "id too long",
+			tableName: "users",
+			args: &args {
+				id: math.MaxInt32 + 1,
+			}
+			wantErr: true
+		},
+		{
+			name: "id empty",
+			tableName: "users",
+			args: &args{
+				id: "",
+			}
+			wantErr: true
+		}
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var s sqlmapper.Mapper
+			if tt.testForEmptyTable {
+				cfgEmpty, clearDB := utilTest.CreateConfig(t)
+				defer clearDB()
+
+				// migrate tables
+				err := utilTest.MigrateTables(cfgEmpty.DB())
+				if err != nil {
+					t.Fatalf("Failed to migrate table by error %v", err)
+				}
+				s = NewPGStore(cfgEmpty.DB(), tt.tableName, cols, cfgEmpty.ModelList)
+			} else {
+				s = NewPGStore(cfg.DB(), tt.tableName, cols, cfg.ModelList)
+			}
+
+			got, err := s.FindByColumnName(tt.args.id)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("pgStore.Delete() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if len(got) != len(tt.want) {
+				t.Errorf("len(got)=%v != len(tt.want)=%v", len(got), len(tt.want))
+				return
+			}
+
+			for i := 0; i < len(got); i++ {
+				// convert data
+				iId, err := utilReflect.ConvertFromInterfacePtr(got[i]["id"].Data)
+				if err != nil {
+					t.Fatal(err)
+				}
+				id := iId.(int)
+				iName, err := utilReflect.ConvertFromInterfacePtr(got[i]["name"].Data)
+				if err != nil {
+					t.Fatal(err)
+				}
+				name := iName.(string)
+
+				if len(got) != len(tt.want) ||
+					id != tt.want[i].Id ||
+					name != tt.want[i].Name {
+					t.Errorf("pgStore.Delete() = %v, want %v", got, tt.want)
 				}
 			}
 		})

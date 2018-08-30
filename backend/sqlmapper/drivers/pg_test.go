1310734f2cda9986eff524c056a6af3e8535f68b
Phan Hieu <hieutrunglephan@gmail.com>
2018-08-30 11:17:24 +0700

    testing for Query

--- backend/sqlmapper/drivers/pg_test.go
+++ backend/sqlmapper/drivers/pg_test.go
@@ -7,10 +7,13 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/jinzhu/gorm"
+
 	"github.com/dwarvesf/smithy/backend/sqlmapper"
 	"github.com/dwarvesf/smithy/common/database"
+	utilDB "github.com/dwarvesf/smithy/common/utils/database/pg"
 	utilTest "github.com/dwarvesf/smithy/common/utils/database/pg/test/set1"
-	"github.com/jinzhu/gorm"
+	utilReflect "github.com/dwarvesf/smithy/common/utils/reflect"
 )
 
 /**
@@ -439,6 +442,200 @@ func Test_SyncAgent(t *testing.T) {
 // 	}
 // }
 
+func Test_pgStore_Query(t *testing.T) {
+	t.Parallel()
+	cfg, clearDB := utilTest.CreateConfig(t)
+	defer clearDB()
+
+	// migrate tables
+	err := utilTest.MigrateTables(cfg.DB())
+	if err != nil {
+		t.Fatalf("Failed to migrate table by error %v", err)
+	}
+
+	//create sample data
+	users, err := utilTest.CreateUserSampleData(cfg.DB())
+	if err != nil {
+		t.Fatalf("Failed to create sample data by error %v", err)
+	}
+
+	type fields struct {
+		db       *gorm.DB
+		modelMap map[string]database.Model
+	}
+	tests := []struct {
+		name              string
+		args              *sqlmapper.Query
+		want              []string
+		want1             []utilDB.User
+		wantErr           bool
+		testForEmptyTable bool
+	}{
+		{
+			name: "Valid test case",
+			args: &sqlmapper.Query{
+				SourceTable: "users",
+				Fields:      []string{"id", "name"},
+				Filter: sqlmapper.Filter{
+					Operator:   "=",
+					ColumnName: "id",
+					Value:      1,
+				},
+			},
+			want:  []string{"id", "name"},
+			want1: users[0:1],
+		},
+		{
+			name: "empty table",
+			args: &sqlmapper.Query{
+				SourceTable: "users",
+				Fields:      []string{"id", "name"},
+			},
+			want:              []string{"id", "name"},
+			want1:             []utilDB.User{},
+			testForEmptyTable: true,
+		},
+		{
+			name: "Find all",
+			args: &sqlmapper.Query{
+				SourceTable: "users",
+				Fields:      []string{"id", "name"},
+			},
+			want:  []string{"id", "name"},
+			want1: users,
+		},
+		{
+			name: "id not exists",
+			args: &sqlmapper.Query{
+				SourceTable: "users",
+				Fields:      []string{"id", "name"},
+				Filter: sqlmapper.Filter{
+					Operator:   "=",
+					ColumnName: "id",
+					Value:      111,
+				},
+			},
+			want:    []string{"id", "name"},
+			wantErr: true,
+		},
+		{
+			name: "id too long",
+			args: &sqlmapper.Query{
+				SourceTable: "users",
+				Fields:      []string{"id", "name"},
+				Filter: sqlmapper.Filter{
+					Operator:   "=",
+					ColumnName: "id",
+					Value:      math.MaxInt32 + 1,
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid column name",
+			args: &sqlmapper.Query{
+				SourceTable: "users",
+				Fields:      []string{"id", "name"},
+				Filter: sqlmapper.Filter{
+					Operator:   "=",
+					ColumnName: "namemmm",
+					Value:      "hieudeptrai1",
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid table name",
+			args: &sqlmapper.Query{
+				SourceTable: "usershandsome",
+				Fields:      []string{"id", "name"},
+				Filter: sqlmapper.Filter{
+					Operator:   "=",
+					ColumnName: "name",
+					Value:      "hieudeptrai1",
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "offset=3 limit=2",
+			args: &sqlmapper.Query{
+				SourceTable: "users",
+				Fields:      []string{"id", "name"},
+				Offset:      3,
+				Limit:       2,
+			},
+			want:  []string{"id", "name"},
+			want1: users[3:5],
+		},
+		{
+			name: "offset=3 limit=0",
+			args: &sqlmapper.Query{
+				SourceTable: "users",
+				Fields:      []string{"id", "name"},
+				Offset:      3,
+			},
+			want:  []string{"id", "name"},
+			want1: users[3:],
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var s sqlmapper.Mapper
+			if tt.testForEmptyTable {
+				cfgEmpty, clearDB := utilTest.CreateConfig(t)
+				defer clearDB()
+
+				// migrate tables
+				err := utilTest.MigrateTables(cfgEmpty.DB())
+				if err != nil {
+					t.Fatalf("Failed to migrate table by error %v", err)
+				}
+				s = NewPGStore(cfgEmpty.DB(), cfgEmpty.ModelList)
+			} else {
+				s = NewPGStore(cfg.DB(), cfg.ModelList)
+			}
+
+			got, got1, err := s.Query(*tt.args)
+			if err != nil {
+				if !tt.wantErr {
+					t.Errorf("pgStore.Query() error = %v, wantErr %v", err, tt.wantErr)
+				}
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("pgStore.Query() got = %v, want %v", got, tt.want)
+			}
+
+			if len(got1) != len(tt.want1) {
+				t.Errorf("len(got1)=%v != len(tt.want1)=%v", len(got1), len(tt.want1))
+				return
+			}
+
+			for i := 0; i < len(got1); i++ {
+				u := got1[i].([]interface{})
+
+				// convert data
+				iId, err := utilReflect.ConvertFromInterfacePtr(u[0])
+				if err != nil {
+					t.Fatal(err)
+				}
+				id := iId.(int)
+				iName, err := utilReflect.ConvertFromInterfacePtr(u[1])
+				if err != nil {
+					t.Fatal(err)
+				}
+				name := iName.(string)
+
+				if id != tt.want1[i].ID ||
+					name != tt.want1[i].Name {
+					t.Errorf("pgStore.FindByColumnName() = %v, want %v", got, tt.want)
+				}
+			}
+		})
+	}
+}
+
 func Test_pgStore_Delete(t *testing.T) {
 	t.Parallel()
 	cfg, clearDB := utilTest.CreateConfig(t)
@@ -702,42 +899,6 @@ func Test_pgStore_Update(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("pgStore.Update() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_pgStore_Query(t *testing.T) {
-	type fields struct {
-		db       *gorm.DB
-		modelMap map[string]database.Model
-	}
-	tests := []struct {
-		name    string
-		args    sqlmapper.Query
-		want    []string
-		want1   []interface{}
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			s := NewPGStore()
-			s := &pgStore{
-				db:       tt.fields.db,
-				modelMap: tt.fields.modelMap,
-			}
-			got, got1, err := s.Query(tt.args.q)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("pgStore.Query() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("pgStore.Query() got = %v, want %v", got, tt.want)
-			}
-			if !reflect.DeepEqual(got1, tt.want1) {
-				t.Errorf("pgStore.Query() got1 = %v, want %v", got1, tt.want1)
 			}
 		})
 	}
